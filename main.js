e9200d07373f79d2eaa9edac8c9f02f9ae883feb
Slindo <sngcobo@dvtsoftware.com>
2025-01-28 16:59:34 +0200

    To complete the tax calculation function, that you need to do at house

--- main.js
+++ main.js
@@ -14,6 +14,8 @@ function showError(msg)
     }, 5000);
 }
 
+let taxSum = 0;
+
 if(taxIncome < 0)
 {
     //Output an error message(Cann't input a negative number)
@@ -23,4 +25,21 @@ if(taxIncome < 0)
 {
     //Output an error message(Can not be empty or a word)
     showError('Please input a number');
+}else if(taxIncome >= 0 && taxIncome <= 10000)
+{
+    //Calculating tax owed at 0%
+    taxSum = taxIncome * (0/100);
+}else if(taxIncome > 10000 && taxIncome <= 20000)
+{
+    //1st bracket results at 0%
+    //10000*(0/100) = 0
+    let taxBrac = 0;
+    //Calculating tax owed at 10%
+    let taxStart = taxIncome - 10000;
+    taxStart = taxStart * (10/100);
+
+    taxSum = taxBrac + taxStart;
+}else if(taxIncome > 20000 && taxIncome <= 50000)
+{
+    
 }
